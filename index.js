0103a0767f5c1c3dbc26292e010670801bf5ee9e
UbaydulloH00 <turonarsloni00@gmail.com>
2023-10-20 20:18:14 +0500

    end

---
+++ index.js
@@ -0,0 +1,60 @@
+
+// function massiv(arr,a) {
+// let arr = [1,2,3,4,5,6,7]
+// let a = 5;  
+// for(i = 1; i=a; i++){
+//     arr = arr.shift()
+// }
+// }
+
+
+// n sonidan m sonigacha raqamlara kopaytmasida nechta nol borligin topish
+// function counterZero(n,m) {
+//     
+// let a = 5;
+// let b = 48;
+// let sum = 1;
+// let count = 0;
+// let zero ;
+// for (i = a; i <= b; i++){
+//     sum += i
+    
+// }
+// let sum1 = sum;
+// for (g = 1; g<=sum1; g++ ){
+//         zero = sum % 10 
+//         if ( zero == 0){
+//             count++
+//         }
+//         sum = Math.trunc(sum /10)
+//     }
+// }
+
+
+// 3 misol ikkita massivni birlashtirish
+// function birlashtirish(a,b) {
+//     const arrayNumber = [1,23,456];
+// const arrayNumber1 = [7,8,9,1];
+// let allNumber;
+// if( arrayNumber.length > arrayNumber1.length ){
+//       allNumber = arrayNumber.concat(arrayNumber1)
+// }else{
+//     allNumber = arrayNumber1.concat(arrayNumber)
+// }
+// console.log(allNumber);
+    
+// }
+
+
+// 4 mislo 
+// function indexlarOrtasidagiYigindi(a,b,c) {
+//     const arr = [1,2,3,4,5,6,7,8,9]
+// let a = 1;
+// let b = 4;
+// let sum = 0;
+// for (i=arr[a];i<=arr[b];i++){
+//     sum += i
+// }
+// console.log(sum);
+    
+// }
